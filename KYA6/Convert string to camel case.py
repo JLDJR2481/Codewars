3f409b61fd814bc8d3cf637f139aaade1ab4e6c8
JLDJR2481 <jjesusramon@cifpfbmoll.eu>
2022-11-22 17:38:28 +0100

    Convert string to camel case

---
+++ KYA6/Convert string to camel case.py
@@ -0,0 +1,9 @@
+def to_camel_case(text):
+    return text[:1] + text.title()[1:].replace('_', '').replace('-', '')
+
+if __name__ == "__main__":
+
+        assert to_camel_case("") == ""  
+        assert to_camel_case("the_stealth_warrior") == "theStealthWarrior" 
+        assert to_camel_case("The-Stealth-Warrior") == "TheStealthWarrior" 
+        assert to_camel_case("A-B-C") == "ABC"
