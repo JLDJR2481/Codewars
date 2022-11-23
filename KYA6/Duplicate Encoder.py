e40728a6fef9639fb1a674da88a0b621e572b18a
JLDJR2481 <jjesusramon@cifpfbmoll.eu>
2022-11-23 18:52:04 +0100

    Duplicate Encoder

---
+++ KYA6/Duplicate Encoder.py
@@ -0,0 +1,23 @@
+def duplicate_encode(string):
+
+    string = string.upper()
+
+    words = {}
+    for letter in string:
+        if letter not in words:
+            words[letter] = '('
+        else:
+            words[letter] = ')'
+
+    result = ""
+    for letter in string:
+        result += words[letter]
+
+    return result
+
+if __name__ == "__main__":
+
+    assert  duplicate_encode("din") == "((("
+    assert  duplicate_encode("recede") == "()()()"
+    assert  duplicate_encode("Success") == ")())())"
+    assert  duplicate_encode("(( @") == "))(("
