c1ac71a42f840ce35e05c0f889aaf51aad35dda1
JLDJR2481 <jjesusramon@cifpfbmoll.eu>
2022-11-23 18:54:07 +0100

    Is he gonna survive?

---
+++ KYA8/Is he gonna survive.py
@@ -0,0 +1,11 @@
+def hero(bullets, dragon):
+    return bullets >= dragon*2
+
+if __name__ == "__main__":
+    
+    assert hero(10, 5) == True
+    assert hero(7, 4) == False
+    assert hero(4, 5) == False
+    assert hero(100, 40) == True
+    assert hero(1500, 751) == False
+    assert hero(0, 1) == False
