2ad13b4246a1f07799cecf6fed9212e48f3c48f4
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2022-12-03 19:32:21 +0100

    Opposite Number

---
+++ KYA8/Opposite Number.py
@@ -0,0 +1,12 @@
+def opposite(number):
+
+    return number * -1
+
+
+if __name__ == "__main__":
+    assert opposite(1) == -1
+    assert opposite(25.6) == -25.6
+    assert opposite(0) == 0
+    assert opposite(1425.2222) == -1425.2222
+    assert opposite(-3.1458) == 3.1458
+    assert opposite(-95858588225) == 95858588225
