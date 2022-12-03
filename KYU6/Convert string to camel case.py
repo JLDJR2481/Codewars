82874d39ea8fa0eaa88b80cf3994f9788ac36c1d
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2022-12-03 20:16:47 +0100

    Convert string to camel case

--- KYU6/Convert string to camel case.py
+++ KYU6/Convert string to camel case.py
@@ -1,9 +1,10 @@
 def to_camel_case(text):
     return text[:1] + text.title()[1:].replace('_', '').replace('-', '')
 
+
 if __name__ == "__main__":
 
-        assert to_camel_case("") == ""  
-        assert to_camel_case("the_stealth_warrior") == "theStealthWarrior" 
-        assert to_camel_case("The-Stealth-Warrior") == "TheStealthWarrior" 
-        assert to_camel_case("A-B-C") == "ABC"
+    assert to_camel_case("") == ""
+    assert to_camel_case("the_stealth_warrior") == "theStealthWarrior"
+    assert to_camel_case("The-Stealth-Warrior") == "TheStealthWarrior"
+    assert to_camel_case("A-B-C") == "ABC"
